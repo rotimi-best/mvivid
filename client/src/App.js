a8315ea54f426d6a8f0cc08b6b515c1a4ba89349
Rotimi-Best <rotimiibitoyebest@yahoo.co.uk>
2019-04-27 14:11:51 +0300

    added scripts and create-react-app

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to mvivid</h1>
+    </div>
+  );
+}
+
+export default App;
