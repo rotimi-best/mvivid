4e7d72ccd5721547dfd4cfe97cc816d5ebb6f4e0
Rotimi-Best <rotimiibitoyebest@yahoo.co.uk>
2019-04-29 11:57:46 +0300

    server init setup

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const { PORT = 5000 } = process.env;
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use("api/auth", require("routes/auth"));
+app.use("api/jobs", require("routes/jobs"));
+app.use("api/people", require("routes/people"));
+app.use("api/form", require("routes/forum"));
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
