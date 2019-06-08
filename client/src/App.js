8b86b8e971267429d72d4505c2674fc4a0120ee2
Rotimi Best <rotimiibitoyebest@yahoo.co.uk>
2019-06-08 12:34:45 +0300

    [Front] Begin redux setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import NotSignedIn from "./components/navigation/NotSignedIn";
 import Home from "./components/home";
@@ -11,25 +12,29 @@ import ForumProfile from "./components/forum/profile";
 import EmployerDashboard from "./components/employer/EmployerDashboard";
 import Authenticate from "./components/auth";
 import Footer from "./components/footer";
+
+import store from "./store";
 import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <NotSignedIn />
-        <Route exact path="/" component={Home} />
-        <Route exact path="/jobs" component={JobSearch} />
-        <Route exact path="/jobs/:id" component={JobProfile} />
-        <Route exact path="/people" component={PeopleSearch} />
-        <Route exact path="/people/:id" component={PeopleProfile} />
-        <Route exact path="/forum" component={ForumSearch} />
-        <Route exact path="/forum/:id" component={ForumProfile} />
-        <Route path="/login" component={Authenticate} />
-        <Route exact path="/employer" component={EmployerDashboard} />
-        <Footer />
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <NotSignedIn />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/jobs" component={JobSearch} />
+          <Route exact path="/jobs/:id" component={JobProfile} />
+          <Route exact path="/people" component={PeopleSearch} />
+          <Route exact path="/people/:id" component={PeopleProfile} />
+          <Route exact path="/forum" component={ForumSearch} />
+          <Route exact path="/forum/:id" component={ForumProfile} />
+          <Route path="/login" component={Authenticate} />
+          <Route exact path="/employer" component={EmployerDashboard} />
+          <Footer />
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
